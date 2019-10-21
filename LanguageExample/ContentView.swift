39a6fc5028c536dc3a8e447043880cb07e72403b
Hannes Oud <hannes.oud.dev@gmail.com>
2019-10-21 11:47:15 +0200

    Initial Commit

---
+++ LanguageExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LanguageExample
+//
+//  Created by Hannes Oud on 21.10.19.
+//  Copyright © 2019 Hannes Oud EPU. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
